c7c9e5ef3d850d341152cd2b3aba0e76f760e355
Fayaz Yusuf Khan <fayaz.yusuf.khan@gmail.com>
2025-06-16 14:08:55 -0400

    Add docstrings

--- main.py
+++ main.py
@@ -2,18 +2,22 @@
 # requires-python = ">=3.13"
 # dependencies = []
 # ///
-
+"""This script implements a simple HTTP server that forwards requests to a specified backend server.
+It reads incoming HTTP requests, forwards them to the backend, and returns the response to the client.
+"""
 import argparse
 import asyncio
 import urllib.parse
 
 
 class Server:
+    """Proxy server implementation"""
     def __init__(self, backend):
         print(f"Using backend: {backend}")
         self.backend_url = urllib.parse.urlparse(backend)
 
     async def serve_forever(self):
+        """Starts the proxy server."""
         server = await asyncio.start_server(
             self._handle_request, '127.0.0.1', 8888)
 
@@ -24,6 +28,7 @@ class Server:
             await server.serve_forever()
 
     async def _handle_request(self, reader, writer):
+        """Handles incoming requests."""
         request = await self._read_message(reader)
         addr = writer.get_extra_info('peername')
         print(f"Received {request!r} from {addr!r}")
@@ -37,12 +42,13 @@ class Server:
         await writer.wait_closed()
 
     async def _read_message(self, reader):
+        """Reads a complete HTTP message from the reader."""
         message = b''
         content_length = None
         # Read the request line and headers
         while True:
             line = await reader.readline()
-            print(f"Read line: {line!r}")
+            print(f"Read header: {line!r}")
             if line.startswith(b'Content-Length:'):
                 # If Content-Length is present, read the specified number of bytes
                 content_length = int(line.split(b':', 1)[1].strip())
@@ -57,7 +63,7 @@ class Server:
         return message
 
     async def _forward_request(self, request):
-        print(f"Forwarding request to {self.backend_url}")
+        """Forwards the request to the backend server."""
         updated_request = request.replace(
             b'Host: 127.0.0.1:8888', f'Host: {self.backend_url.hostname}:{self.backend_url.port}'.encode())
         print(f"Sending updated request: {updated_request!r}")
@@ -83,4 +89,3 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
-
